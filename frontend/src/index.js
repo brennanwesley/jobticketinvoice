6d5de76965f66df7c0e6737d1f438185bc0f3f4d
brennantharaldson <brennan.tharaldson@intrinsii.com>
2025-06-15 13:28:26 -0500

    Fix 401 authentication errors - remove test auth import from production
    - Remove test authentication utilities import from index.js
    - Prevent automatic loading of test functions that could cause auth issues
    - This should resolve the repeated 401 errors on /auth/login endpoint
    - Test utilities are still available for manual testing when needed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,6 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-// Import auth test utilities in development mode
-if (process.env.NODE_ENV === 'development') {
-  import('./utils/test-auth')
-    .then(() => console.log('Auth test utilities loaded. Use testLogin(), testRegister(), etc. in console.'))
-    .catch(err => console.error('Failed to load auth test utilities:', err))
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
